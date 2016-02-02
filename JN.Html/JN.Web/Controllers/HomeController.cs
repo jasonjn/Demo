bf35ca938d2ae20dacb2120858ce52c0cf77644c
jiangnan <26533875@qq.com>
2016-02-02 17:42:51 +0800

    uploadDemo

---
+++ JN.Html/JN.Web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace JN.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Upload()
+        {
+            var file = Request.InputStream ;
+
+            var memoryStream = new MemoryStream();
+            file.CopyTo(memoryStream);
+
+            System.IO.File.WriteAllBytes(@"D:\img", memoryStream.ToArray());
+
+            return View("Index");
+        }
+    }
+}
