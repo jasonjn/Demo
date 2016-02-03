94f24bdad6289b888361345d9cf1742e3bc2fb92
jiangnan <26533875@qq.com>
2016-02-03 10:53:10 +0800

    还原

---
+++ JN.Demo/JN.Demo/FileAccessDemo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JN.Demo
+{
+    class FileAccessDemo
+    {
+        public static void test1()
+        {
+           
+            try
+            {
+                var filepath = @"D:\testest";   //test        
+                var fileSecurity = File.GetAccessControl(filepath);             
+                fileSecurity.AddAccessRule(new FileSystemAccessRule("Users", FileSystemRights.FullControl, AccessControlType.Allow));          
+                File.SetAccessControl(filepath,fileSecurity);
+            }
+            catch  
+            {
+               
+            }
+    
+           
+            
+        }
+    }
+}
