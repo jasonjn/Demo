c5b408d661796ede210a5c2530b719b03a8bd26e
jiangnan <26533875@qq.com>
2016-03-18 08:36:48 +0800

    新增winform 的 demo

---
+++ JN.Demo/JN.Demo.WinForm/FormManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JN.Demo.WinForm
+{
+   public class FormManager
+    {
+
+        public static void Show(Form form)
+        {
+            Form1.ActiveForm.Hide();
+            form.Show();
+        }
+        public static void Return()
+        {
+            Form1.ActiveForm.Hide();
+            new Form1().Show();
+        }
+
+
+        public static void Excute(Action action)
+        {
+            action();
+        }
+    }
+}
---
+++ JN.Demo/JN.Demo.WinForm/Test1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JN.Demo.WinForm
+{
+    public partial class Test1 : Form
+    {
+        public Test1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormManager.Excute(() =>
+            {
+                ShowText();
+                AsyncShow();
+            });
+        }
+
+        public async void AsyncShow()
+        {
+            //Thread thread = new Thread(ShowText);
+            //thread.Start();            
+
+            await  Task.Run(()=>ShowText());
+        }
+
+        private void ShowText()
+        {
+            if (this.InvokeRequired)
+            {
+                Action action = () => { listBox1.Items.Add("这是分线程的添加"); };
+                this.Invoke(action);
+            }
+            else
+            {
+                listBox1.Items.Add("这是主线程的添加");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FormManager.Return();
+        }
+    }
+}
